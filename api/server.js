bb6c114b68c44748b130d477ff054c4e05b88026
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-02-17 16:28:13 +0300

    edit

--- api/server.js
+++ api/server.js
@@ -11,18 +11,18 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-app.use('/categorie', require('./routers/categorie'));
-app.use('/order', require('./routers/order'));
-app.use('/product', require('./routers/product'));
-app.use('/role', require('./routers/role'));
-app.use('/user', require('./routers/user'));
-
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+app.use('/categorie', require('./routers/categorie'));
+app.use('/order', require('./routers/order'));
+app.use('/product', require('./routers/product'));
+app.use('/role', require('./routers/role'));
+app.use('/user', require('./routers/user'));
 
 app.get('/', (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
