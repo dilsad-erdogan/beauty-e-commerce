5c0d6f2d47fc39eba62d9d97c5f642f2dc234e3e
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-02-26 20:44:19 +0300

    services

---
+++ api/services/categorie.js
@@ -0,0 +1,75 @@
+import axios from 'axios';
+const CATEGORIE = "http://localhost:3000/categorie";
+
+const add = async (data) => {
+    try{
+        const response = await axios.post(`${CATEGORIE}/set`, data);
+        return response.data;
+    } catch (error){
+        console.error('Error adding categorie:', error);
+        throw error;
+    }
+};
+
+const get = async () => {
+    try{
+        const response = await axios.get(`${CATEGORIE}/get`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching categorie:', error);
+        throw error;
+    }
+};
+
+const byId = async (id) => {
+    try{
+        const response = await axios.get(`${CATEGORIE}/getById/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching categorie by id:', error);
+        throw error;
+    }
+};
+
+const updateName = async (id, name) => {
+    try{
+        const response = await axios.put(`${CATEGORIE}/updateName/${id}`, name);
+        return response.data;
+    } catch (error){
+        console.error('Error updating categorie:', error);
+        throw error;
+    }
+};
+
+const updateDescription = async (id, description) => {
+    try{
+        const response = await axios.put(`${CATEGORIE}/updateDescription/${id}`, description);
+        return response.data;
+    } catch (error){
+        console.error('Error updating categorie:', error);
+        throw error;
+    }
+};
+
+const updateImage = async (id, image) => {
+    try{
+        const response = await axios.put(`${CATEGORIE}/updateImage/${id}`, image);
+        return response.data;
+    } catch (error){
+        console.error('Error updating categorie:', error);
+        throw error;
+    }
+};
+
+const deleted = async (id) => {
+    try{
+        const response = await axios.patch(`${CATEGORIE}/delete/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error deleting categorie:', error);
+        throw error;
+    }
+};
+
+const categorieServices = { add, get, byId, updateName, updateDescription, updateImage, deleted };
+export default categorieServices;
---
+++ api/services/product.js
@@ -0,0 +1,95 @@
+import axios from 'axios'
+const PRODUCT = "http://localhost:3000/product"
+
+const add = async (data) => {
+    try{
+        const response = await axios.post(`${PRODUCT}/set`, data);
+        return response.data;
+    } catch (error){
+        console.error('Error adding product:', error);
+        throw error;
+    }
+};
+
+const get = async () => {
+    try{
+        const response = await axios.get(`${PRODUCT}/get`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching product:', error);
+        throw error;
+    }
+};
+
+const byId = async (id) => {
+    try{
+        const response = await axios.get(`${PRODUCT}/getById/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching product by id:', error);
+        throw error;
+    }
+};
+
+const updateCatId = async (id, cat_id) => {
+    try{
+        const response = await axios.put(`${PRODUCT}/updateCatId/${id}`, cat_id);
+        return response.data;
+    } catch (error){
+        console.error('Error updating product time:', error);
+        throw error;
+    }
+};
+
+const updateName = async (id, name) => {
+    try{
+        const response = await axios.put(`${PRODUCT}/updateName/${id}`, name);
+        return response.data;
+    } catch (error){
+        console.error('Error updating product time:', error);
+        throw error;
+    }
+};
+
+const updateDescription = async (id, description) => {
+    try{
+        const response = await axios.put(`${PRODUCT}/updateDescription/${id}`, description);
+        return response.data;
+    } catch (error){
+        console.error('Error updating product time:', error);
+        throw error;
+    }
+};
+
+const updatePrice = async (id, price) => {
+    try{
+        const response = await axios.put(`${PRODUCT}/updatePrice/${id}`, price);
+        return response.data;
+    } catch (error){
+        console.error('Error updating product time:', error);
+        throw error;
+    }
+};
+
+const updateImage = async (id, image) => {
+    try{
+        const response = await axios.put(`${PRODUCT}/updateImage/${id}`, image);
+        return response.data;
+    } catch (error){
+        console.error('Error updating product time:', error);
+        throw error;
+    }
+};
+
+const deleted = async (id) => {
+    try{
+        const response = await axios.patch(`${PRODUCT}/delete/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error deleting product:', error);
+        throw error;
+    }
+};
+
+const productServices = { add, get, byId, updateCatId, updateName, updateDescription, updatePrice, updateImage, deleted };
+export default productServices 
