979c38c830249c60cc7d4b894f9566ad541162c8
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-02-26 21:02:44 +0300

    redux

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import { Provider } from "react-redux"
+import store from "./redux/store.jsx"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
---
+++ web/src/redux/cartSlice.jsx
@@ -0,0 +1,75 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    products: [],
+    totalQuantity: 0,
+    totalPrice: 0
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart(state, action) {
+            const newItem = action.payload;
+            const itemIndex = state.products.find((item) => item._id === newItem._id);
+            if(itemIndex) {
+                itemIndex.quantity++;
+                itemIndex.totalPrice += newItem.totalPrice;
+            } else {
+                state.products.push({
+                    _id: newItem._id,
+                    name: newItem.name,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    image: newItem.image
+                })
+            }
+
+            state.totalPrice += newItem.price;
+            state.totalQuantity++;
+        },
+
+        removeFromCart(state, action) {
+            const id = action.payload;
+            const findItem = state.products.find((item) => item._id === id);
+            if(findItem) {
+                state.totalPrice -= findItem.totalPrice;
+                state.totalQuantity -= findItem.quantity;
+                state.products = state.products.filter(item => item._id !== id);
+            }
+        },
+
+        decreaseQuantity(state, action) {
+            const id = action.payload;
+            const findItem = state.products.find((item) => item._id === id);
+            if(findItem) {
+                if(findItem.quantity > 1){
+                    findItem.quantity -= 1;
+                    state.totalPrice -= findItem.price;
+                    state.totalQuantity -= 1;
+                }
+            }
+        },
+        
+        increaseQuantity(state, action) {
+            const id = action.payload;
+            const findItem = state.products.find((item) => item._id === id);
+            if(findItem) {
+                findItem.quantity += 1;
+                state.totalPrice += findItem.price;
+                state.totalQuantity += 1;
+            }
+        },
+
+        dropCart(state) {
+            state.products = []
+            state.totalPrice = 0
+            state.totalQuantity = 0
+        }
+    },
+})
+
+export const { addToCart, removeFromCart, increaseQuantity, decreaseQuantity, dropCart } = cartSlice.actions
+export default cartSlice.reducer
