0f3e18922b741dd6090f13a42090dd3cbd37f34c
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-03-05 17:00:10 +0300

    navigator mobile

--- mobile/src/navigator/AppNavigator.js
+++ mobile/src/navigator/AppNavigator.js
@@ -3,6 +3,12 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView, View, StyleSheet } from "react-native";
 import HomeScreen from "../screens/HomeScreen";
+import ContactUsScreen from "../screens/ContactUsScreen";
+import GiftCardScreen from "../screens/GiftCardScreen";
+import OrderScreen from "../screens/OrderScreen";
+import SearchScreen from "../screens/SearchScreen";
+import ServiceScreen from "../screens/ServiceScreen";
+import ShopScreen from "../screens/ShopScreen";
 import Navbar from "../components/bars/Navbar";
 import Footer from "../components/bars/Footer";
 
@@ -21,6 +27,12 @@ const AppNavigator = () => {
         <View style={styles.contentContainer}>
           <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="ContactUs" component={ContactUsScreen} />
+            <Stack.Screen name="GiftCard" component={GiftCardScreen} />
+            <Stack.Screen name="Order" component={OrderScreen} />
+            <Stack.Screen name="Search" component={SearchScreen} />
+            <Stack.Screen name="Service" component={ServiceScreen} />
+            <Stack.Screen name="Shop" component={ShopScreen} />
           </Stack.Navigator>
         </View>
 
