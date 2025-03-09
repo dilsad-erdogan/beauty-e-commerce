14d14e60e4be03b0c41180383c3bbfab393fade5
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-03-10 01:52:25 +0300

    category redux

--- admin/src/redux/store.jsx
+++ admin/src/redux/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import orderSlice from "./orderSlice";
 import productSlice from "./productSlice";
+import categorySlice from "./categorySlice";
 
 const store = configureStore({
     reducer: {
         order: orderSlice,
-        product: productSlice
+        product: productSlice,
+        category: categorySlice,
     }
 });
 
