e3fd4789c32981bacb97dfec4784947c596b57c5
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-03-09 22:25:11 +0300

    redux

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux"
+import store from "../redux/store";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import Main from "../pages/Main";
@@ -12,28 +14,30 @@ import Service from "../pages/Service";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-black text-white">
-      <Router>
-        {/* Navbar */}
-        <Navbar />
+    <Provider store={store}>
+      <div className="h-screen bg-black text-white">
+        <Router>
+          {/* Navbar */}
+          <Navbar />
 
-        <div className="flex w-full h-screen overflow-hidden">
-          <Toaster position="top-right" />
+          <div className="flex w-full h-screen overflow-hidden">
+            <Toaster position="top-right" />
 
-          {/* Sidebar */}
-          <div className="hidden lg:block w-1/6 p-5 overflow-y-auto bg-gray-700">
-            <Sidebar />
+            {/* Sidebar */}
+            <div className="hidden lg:block w-1/6 p-5 overflow-y-auto bg-gray-700">
+              <Sidebar />
+            </div>
+
+            <Routes>
+              <Route path='/' element={<Main />} />
+              <Route path='/category' element={<Category />} />
+              <Route path='/product' element={<Product />} />
+              <Route path='/order' element={<Order />} />
+              <Route path='/service' element={<Service />} />
+            </Routes>
           </div>
-
-          <Routes>
-            <Route path='/' element={<Main />} />
-            <Route path='/category' element={<Category />} />
-            <Route path='/product' element={<Product />} />
-            <Route path='/order' element={<Order />} />
-            <Route path='/service' element={<Service />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+        </Router>
+      </div>
+    </Provider>
   )
 }
