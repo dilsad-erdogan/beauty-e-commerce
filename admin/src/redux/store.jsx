852b2a736ec07c716d475166d44413113d61d063
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-03-10 21:40:44 +0300

    service redux

--- admin/src/redux/store.jsx
+++ admin/src/redux/store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import orderSlice from "./orderSlice";
 import productSlice from "./productSlice";
 import categorySlice from "./categorySlice";
+import serviceSlice from "./serviceSlice";
 
 const store = configureStore({
     reducer: {
         order: orderSlice,
         product: productSlice,
         category: categorySlice,
+        service: serviceSlice
     }
 });
 
