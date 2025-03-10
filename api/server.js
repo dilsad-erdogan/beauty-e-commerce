b94f33a6ce4323e80d94d6dfe7198141b1cbf408
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-03-10 21:49:52 +0300

    api port

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/mongoDB');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 connectDB();
 
